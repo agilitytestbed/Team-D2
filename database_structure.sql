2867615bfd137838af793e8cc789e6f1b37194b0
Sammz <sam.witt25@gmail.com>
2018-07-19 22:07:58 +0200

    Add category_rule and description field to database

--- database_structure.sql
+++ database_structure.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS User_Table(
   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   session_id TEXT,
   highest_transaction_id BIGINT,
-  highest_category_id BIGINT
+  highest_category_id BIGINT,
+  highest_category_rule_id BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS Transaction_Table(
@@ -10,6 +11,7 @@ CREATE TABLE IF NOT EXISTS Transaction_Table(
   transaction_id BIGINT,
   date DATETIME,
   amount FLOAT,
+  description TEXT,
   external_iban TEXT,
   type TEXT,
   FOREIGN KEY(user_id) REFERENCES User_Table(user_id),
@@ -22,6 +24,19 @@ CREATE TABLE IF NOT EXISTS Category_Table(
   name TEXT,
   FOREIGN KEY(user_id) REFERENCES User_Table(user_id),
   PRIMARY KEY(user_id, category_id)
+);
+
+CREATE TABLE IF NOT EXISTS CategoryRule_Table(
+  user_id INTEGER,
+  category_rule_id BIGINT,
+  description TEXT,
+  iban TEXT,
+  type TEXT,
+  category_id BIGINT,
+  apply_on_history BOOLEAN,
+  FOREIGN KEY(user_id) REFERENCES User_Table(user_id),
+  FOREIGN KEY(category_id) REFERENCES Category_Table(category_id),
+  PRIMARY KEY(user_id, category_rule_id)
 );
 
 CREATE TABLE IF NOT EXISTS Transaction_Category(
